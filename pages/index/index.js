eeb81d3cb900575058f6284ad795cf1f8be8a824
username <565368208@qq.comm>
2018-06-19 18:09:37 +0800

    项目初建

---
+++ pages/index/index.js
@@ -0,0 +1,195 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    // motto: 'Hello World',
+    // userInfo: {},
+    // hasUserInfo: false,
+    // canIUse: wx.canIUse('button.open-type.getUserInfo')
+
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    indicatorColor: '#96a6a6',
+    indicatorActiveColor: '#f8f8f8',
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
+
+    latitude: "",
+    longitude: "",
+
+
+    // markers: [{
+    //   iconPath: "/resources/others.png",
+    //   id: 0,
+    //   latitude: 23.099994,
+    //   longitude: 113.324520,
+    //   width: 50,
+    //   height: 50
+    // }],
+    // polyline: [{
+    //   points: [{
+    //     longitude: 113.3245211,
+    //     latitude: 23.10229
+    //   }, {
+    //     longitude: 113.324520,
+    //     latitude: 23.21229
+    //   }],
+    //   color: "#FF0000DD",
+    //   width: 2,
+    //   dottedLine: true
+    // }],
+    // controls: [{
+    //   id: 1,
+    //   iconPath: '/resources/location.png',
+    //   position: {
+    //     left: 0,
+    //     top: 300 - 50,
+    //     width: 50,
+    //     height: 50
+    //   },
+    //   clickable: true
+    // }]
+  },
+  regionchange(e) {
+    console.log(e.type)
+  },
+  markertap(e) {
+    console.log(e.markerId)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  }
+  ,
+  //事件处理函数
+  // bindViewTap: function() {
+  //   wx.navigateTo({
+  //     url: '../logs/logs'
+  //   })
+  // },
+  onLoad: function () {
+    // if (app.globalData.userInfo) {
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // } else if (this.data.canIUse){
+    //   // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //   // 所以此处加入 callback 以防止这种情况
+    //   app.userInfoReadyCallback = res => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // } else {
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   wx.getUserInfo({
+    //     success: res => {
+    //       app.globalData.userInfo = res.userInfo
+    //       this.setData({
+    //         userInfo: res.userInfo,
+    //         hasUserInfo: true
+    //       })
+    //     }
+    //   })
+    // }
+    wx.getSetting({
+      success: (res) => {
+        console.log(res);
+
+        if (res.authSetting['scope.userLocation'] != undefined && res.authSetting['scope.userLocation'] != true) {//非初始化进入该页面,且未授权
+          wx.showModal({
+            title: '是否授权当前位置',
+            content: '需要获取您的地理位置，请确认授权，否则无法获取您所需数据',
+            success: function (res) {
+              if (res.cancel) {
+                that.setData({
+                  isshowCIty: false
+                })
+                wx.showToast({
+                  title: '授权失败',
+                  icon: 'success',
+                  duration: 1000
+                })
+              } else if (res.confirm) {
+                wx.openSetting({
+                  success: function (dataAu) {
+                    if (dataAu.authSetting["scope.userLocation"] == true) {
+                      wx.showToast({
+                        title: '授权成功',
+                        icon: 'success',
+                        duration: 1000
+                      })
+                      //再次授权，调用getLocationt的API
+                      this.getLocation(this);
+                    } else {
+                      wx.showToast({
+                        title: '授权失败',
+                        icon: 'success',
+                        duration: 1000
+                      })
+                    }
+                  }
+                })
+              }
+            }
+          })
+        } else if (res.authSetting['scope.userLocation'] == undefined) {//初始化进入
+          this.getLocation(this);
+        } else { //授权后默认加载
+          this.getLocation(this);
+        }
+
+      }
+    })
+
+  },
+  // getUserInfo: function(e) {
+  //   console.log(e)
+  //   app.globalData.userInfo = e.detail.userInfo
+  //   this.setData({
+  //     userInfo: e.detail.userInfo,
+  //     hasUserInfo: true
+  //   })
+  // },
+  getLocation: (that) => {
+    wx.getLocation({
+      type: 'wgs84', // 默认为 wgs84 返回 gps 坐标，gcj02 返回可用于 wx.openLocation 的坐标  
+      success: (res) => {
+        console.log(res);
+        that.setData({
+          latitude: res.latitude,
+          longitude: res.longitude,
+          // markers: [{
+          //   id: "1",
+          //   latitude: res.latitude,
+          //   longitude: res.longitude,
+          //   width: 50,
+          //   height: 50,
+          //   // iconPath: "../images/my.png",
+          //   title: "我的当前位置"
+          // }],
+          // circles: [{
+          //   latitude: res.latitude,
+          //   longitude: res.longitude,
+          //   color: '#FF0000DD',
+          //   fillColor: '#7cb5ec88',
+          //   radius: 3000,
+          //   strokeWidth: 1
+          // }]
+        })
+      },
+      fail: function (res) {
+        console.log(res);
+      }
+    })
+  }
+
+})
