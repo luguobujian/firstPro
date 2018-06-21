0c56f920d27148029ae838f084ac843b0d6ea7b3
username <565368208@qq.comm>
2018-06-21 18:20:23 +0800

    头条&我的大体实现

--- pages/headline/headline.js
+++ pages/headline/headline.js
@@ -5,62 +5,145 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    winHeight: "", //窗口高度
+    currentTab: 0, //预设当前项的值
+    scrollLeft: 0, //tab标题的滚动条位置
+    expertList: [{ //假数据
+      img: "avatar.png",
+      name: "欢顔",
+      tag: "知名情感博主",
+      answer: 134,
+      listen: 2234
+    }],
+
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: true,
+    indicatorColor: '#96a6a6',
+    indicatorActiveColor: '#f8f8f8',
+    autoplay: true,
+    interval: 5000,
+    duration: 1000,
   },
+  // 滚动切换标签样式
+  switchTab: function(e) {
+    this.setData({
+      currentTab: e.detail.current
+    });
+    this.checkCor();
+    if (e.detail.current == 0) {
+      this.setData({
+        winHeight: 372 + 224 * 6
+      });
+    } else {
+      this.setData({
+        winHeight: 224 * 6
+      });
+    }
+  },
+  // 点击标题切换当前页时改变样式
+  swichNav: function(e) {
+    var cur = e.target.dataset.current;
+    if (this.data.currentTaB == cur) {
+      return false;
+    } else {
+      if (cur == 0) {
+        this.setData({
+          winHeight: 372 + 224 * 6
+        });
+      } else {
+        this.setData({
+          winHeight: 224 * 6
+        });
+      }
+      this.setData({
+        currentTab: cur
+      })
+    }
+  },
+  //判断当前滚动超过一屏时，设置tab标题滚动条。
+  checkCor: function() {
+    if (this.data.currentTab > 4) {
+      this.setData({
+        scrollLeft: 300
+      })
+    } else {
+      this.setData({
+        scrollLeft: 0
+      })
+    }
+  },
+  onLoad: function() {
+    let that = this;
+    //  高度自适应
+    if (that.data.currentTab == 0) {
+      that.setData({
+        winHeight: 372 + 224 * that.data.imgUrls.length
+      });
+    } else {
+      that.setData({
+        winHeight: 224 * that.data.imgUrls.length
+      });
+    }
+  },
+  // footerTap: app.footerTap,
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
-  },
+  // onLoad: function(options) {
+
+  // },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   }
 })
